d5ee1decf78c18d9b01fbc07b22fdcd41765992f
rlfrahm <frahmryan@gmail.com>
2014-11-07 14:44:21 -0600

    About text, watershed click event

--- app.js
+++ app.js
@@ -1,25 +1,37 @@
 angular.module('pewi', [])
 
-.controller('MainCtrl', ['$scope','$http','$location',
-  function($scope, $http, $location) {
+.value('selectedLandcover', null)
+
+.constant('cellImgs', [
+    "",
+    "Landcover_Corn.png",
+    "Landcover_Conservation_Corn.png",
+    "Landcover_Soybean.png",
+    "Landcover_Conservation_Soybean.png",
+    "Landcover_Alfalfa.png",
+    ["Landcover_Permanent_Pasture.png", "Landcover_Permanent_Pasture_No_Cattle.png"],
+    ["Landcover_Rotational_Grazing.png", "Landcover_Rotational_Grazing_No_Cattle.png"],
+    ["Landcover_Hay.png", "Landcover_Grass_Hay_No_Bales.png"],
+    "Landcover_Prairie.png",
+    "Landcover_Conservation_Forest.png",
+    "Landcover_Conventional_Forest.png",
+    "Landcover_Herbaceous_Bioenergy.png",
+    "Landcover_Woody_Bioenergy.png",
+    "Landcover_Wetland.png",
+    "Landcover_Mixed_Fruit_and_Vegetable.png"]
+)
+
+.controller('MainCtrl', ['$scope','$http','$location', 'selectedLandcover',
+  function($scope, $http, $location, selectedLandcover) {
     $http.get($location.absUrl() + 'data/data.json').success(
       function(response) {
-        console.log(response);
         $scope.data = response.data;
       }
     );
-
+    $scope.selectedLandcover = null;
     $scope.sidenav = true;
     $scope.toggleSidenav = function() {
       $scope.sidenav = !$scope.sidenav;
-    };
-
-    $scope.selectedLandcover = null;
-    $scope.setSelectedLandcover = function(lid) {
-      $scope.selectedLandcover = lid;
-    };
-    $scope.landcoverIsSelected = function(lid) {
-      return ($scope.selectedLandcover == lid);
     };
 
     $scope.year = null;
@@ -31,34 +43,22 @@ angular.module('pewi', [])
     };
 
     $scope.modalDialogTemplate = null;
-
-    $scope.cellImgs =  ["",
-      "Landcover_Corn.png",
-      "Landcover_Conservation_Corn.png",
-      "Landcover_Soybean.png",
-      "Landcover_Conservation_Soybean.png",
-      "Landcover_Alfalfa.png",
-      ["Landcover_Permanent_Pasture.png", "Landcover_Permanent_Pasture_No_Cattle.png"],
-      ["Landcover_Rotational_Grazing.png", "Landcover_Rotational_Grazing_No_Cattle.png"],
-      ["Landcover_Hay.png", "Landcover_Grass_Hay_No_Bales.png"],
-      "Landcover_Prairie.png",
-      "Landcover_Conservation_Forest.png",
-      "Landcover_Conventional_Forest.png",
-      "Landcover_Herbaceous_Bioenergy.png",
-      "Landcover_Woody_Bioenergy.png",
-      "Landcover_Wetland.png",
-      "Landcover_Mixed_Fruit_and_Vegetable.png"];
   }
 ])
 
-.controller('LandCoverSelectCtrl', ['$scope', '$http', '$location',
-  function($scope, $http, $location) {
+.controller('LandCoverSelectCtrl', ['$scope', '$http', '$location', 'selectedLandcover',
+  function($scope, $http, $location, selectedLandcover) {
     $http.get($location.absUrl() + 'data/landcovers.json').success(
       function(response) {
-        console.log(response);
         $scope.landcovers = response;
       }
-    )
+    );
+    $scope.setSelectedLandcover = function(lid) {
+      $scope.$parent.selectedLandcover = lid;
+    };
+    $scope.landcoverIsSelected = function(lid) {
+      return ($scope.$parent.selectedLandcover == lid);
+    };
   }
 ])
 
@@ -66,17 +66,35 @@ angular.module('pewi', [])
   function($scope, $http, $location) {
     $http.get($location.absUrl() + 'data/features.json').success(
       function(response) {
-        console.log(response);
         $scope.features = response;
       }
     )
   }
 ])
 
+.directive('appWatershed', ['selectedLandcover', 'cellImgs',
+    function(selectedLandcover, cellImgs) {
+      return {
+        scope: false,
+        link: function(scope, element, attrs) {
+          //console.log(typeof cellImgs[scope.landcover]);
+          scope.src = (typeof cellImgs[scope.landcover] == 'object') ? cellImgs[scope.landcover][Math.floor(Math.random() * 2)] : cellImgs[scope.landcover];
+          scope.setLandcover = function(rid, landcover) {
+            console.log(scope);
+            scope.landcover = landcover;
+            console.log(landcover);
+            scope.src = (typeof cellImgs[scope.landcover] == 'object') ? cellImgs[scope.landcover][Math.floor(Math.random() * 2)] : cellImgs[scope.landcover];
+          };
+        }
+      }
+    }
+  ]
+)
+
 .directive('appModal', ['$compile',
     function($compile) {
       return {
-        scope: '=',
+        //scope: '=',
         link: function(scope, element, attrs) {
           element.attr('data-toggle', 'modal');
           element.attr('data-target', '#modalDialog');
